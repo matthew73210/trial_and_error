f88de6a86d1a8f587d5a9880c823a25d3a9e8a3c
Matthew Maxwell-Burton <matthewmaxwellburton@gmail.com>
2020-04-24 12:13:18 +0200

    Added webpage

--- trial_and_error/hello/views.py
+++ trial_and_error/hello/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.utils import timezone
+from .models import introduction # import the class
 
 # Create your views here.
 # Where views are created for the hello app
+# Used to link models to tmplates
 
-def home(requests):
-    return render(requests, 'hello/home.html', {}) # Be carefull with path here, dont need hello/templates/hel..
+def home(request):
+    people = introduction.objects.all()
+    return render(request, 'hello/home.html', {'people': people }) # Be carefull with path here, dont need hello/templates/hel..
